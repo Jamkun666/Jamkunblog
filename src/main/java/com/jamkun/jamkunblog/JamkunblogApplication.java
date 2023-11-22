c656deb5aae5f377619d6e8fcf7b0b33ff125ea4
jamkun <846355589@qq.com>
2023-11-23 01:26:51 +0800

    add

---
+++ src/main/java/com/jamkun/jamkunblog/JamkunblogApplication.java
@@ -0,0 +1,13 @@
+package com.jamkun.jamkunblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamkunblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamkunblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamkun/jamkunblog/JamkunblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamkun.jamkunblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamkunblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
